037cc3d0d6fa1232647e1f81ad2f829bb3ceb77c
Derek Chen <derekgc@icloud.com>
2018-11-09 16:43:11 -0500

    update the background

--- src/components/About.js
+++ src/components/About.js
@@ -4,8 +4,8 @@ import selfImage from '../images/derekchen.jpg';
 class About extends Component {
     render() {
         return (
-            <div>
-                <img src={selfImage} className="self-image"/>
+            <div className="front" id="home">
+                <img src={selfImage} className="self-image" width="20%"/>
                 <br></br>
                 About me
                 <p>Full stack web developer with a passion for being a part of the technology revolution.  With experience in Ruby on Rails, JavaScript, and React and a background in electrical and computer engineering, I discovered programing through hacking computer games and programming Ti-83 calculators as a teenager.  I bring strong skills in project management and analytical problem solving that help teams deliver in today’s fast-paced deployment cycles.</p>
