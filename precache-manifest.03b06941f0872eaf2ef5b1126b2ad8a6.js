299e70d02e83ae5fbcc673a88172c557b8f26f28
Derek Chen <derekgc@icloud.com>
2018-11-13 15:03:49 -0500

    updated navbar auto hide

---
+++ precache-manifest.03b06941f0872eaf2ef5b1126b2ad8a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e7ddb1c3f1ef156e5f39d7805e47c3f",
+    "url": "/static/media/derekchen.6e7ddb1c.jpg"
+  },
+  {
+    "revision": "988d73a8f855d0d6388659e8201b54e5",
+    "url": "/static/media/Santorini.988d73a8.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "40af62746d3a2ced4cd4",
+    "url": "/static/js/main.40af6274.chunk.js"
+  },
+  {
+    "revision": "c11bebe6022cf6b7be57",
+    "url": "/static/js/1.c11bebe6.chunk.js"
+  },
+  {
+    "revision": "40af62746d3a2ced4cd4",
+    "url": "/static/css/main.ce1ade67.chunk.css"
+  },
+  {
+    "revision": "8f11f7f8c9a6bdfcaaf22dbbdc6f983e",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cb74a4784bd8b015a44fb7a5d411a89f.js"
+  "/precache-manifest.03b06941f0872eaf2ef5b1126b2ad8a6.js"
 );
 
 workbox.clientsClaim();
