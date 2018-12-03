5842190ef3ec363edd5a97507951eb72d980aa62
Derek Chen <derekgc@icloud.com>
2018-12-03 15:38:42 -0500

    add a new project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import ProjectCard from './ProjectCard';
 import typingGame from '../images/typing.png'
 import stockfinder from '../images/stockfinder.png'
-import play from '../images/play.png'
+import play from '../images/play.png';
+import stockSimulator from '../images/stock-simulator.png'
+import GridCard from './GridCard';
 
 
 import PropTypes from 'prop-types';
@@ -10,18 +12,35 @@ import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 
 const styles = theme => ({
-  root: {
-    flexGrow: 1,
-  },
-});
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing.unit * 2,
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+  });
 
 function Projects(props) {
     const { classes } = props;
         return (
             <div id="home" className="front">
                 <h1 > Projects </h1>
-                <div className={classes.root}>
+                <div >
+
                     <Grid container spacing={24} >
+
+                    <Grid item xs={4}>
+                        <ProjectCard 
+                            title="Trade Simulator"
+                            description="A stock trade application allows users to simulate buy/sell stocks at real time prices."
+                            imageUrl= {stockSimulator}
+                            urls={[{link:"https://github.com/derekgch/trade-simulator", website:"Github"}, 
+                                {link:"https://derekgch.github.io/trade-simulator", website:"Demo website"}]}
+                            />
+
+                    </Grid>
                         <Grid item xs={4}>
                         <ProjectCard 
                             className={classes.paper}
@@ -29,7 +48,7 @@ function Projects(props) {
                             description="A personal finance tool for stock portfolio review and analysis"
                             imageUrl= {stockfinder}
                             urls={[{link:"https://github.com/derekgch/Stock-finder-frontend", website:"github"},
-                            {link:"https://www.youtube.com/watch?v=hIrq1UIZmIo", website:"Demo"}, 
+                            {link:"https://www.youtube.com/watch?v=hIrq1UIZmIo", website:"Demo video"}, 
                             {link:"https://frontend-stockfinder.herokuapp.com/", website:"heroku"}]}
                         />
                         
@@ -40,20 +59,21 @@ function Projects(props) {
                             description=" A web app that helps kids and teens to learn math and English"
                             imageUrl= {play}
                             urls={[{link:"www.youtube.com", website:"Github"},
-                            {link:"https://www.youtube.com/watch?v=HcYxzMOqLIA", website:"Demo"}, 
+                            {link:"https://www.youtube.com/watch?v=HcYxzMOqLIA", website:"Demo video"}, 
                             ]}
                             />
                         </Grid>
-                        
-                        <Grid item xs={4}>
-                        <ProjectCard 
-                            title="Typing Game"
-                            description="A 30-second timed game help people track, practice, and improve their typing skills"
-                            imageUrl= {typingGame}
-                            urls={[{link:"https://github.com/derekgch/typing-game", website:"Github"}, {link:"https://hidden-woodland-52554.herokuapp.com/main.html", website:"Heroku"}]}
-                            />
 
-                    </Grid>
+                        <Grid item xs={4}>
+                            <ProjectCard 
+                                title="Typing Game"
+                                description="A 30-second timed game help people track, practice, and improve their typing skills"
+                                imageUrl= {typingGame}
+                                urls={[{link:"https://github.com/derekgch/typing-game", website:"Github"}, {link:"https://hidden-woodland-52554.herokuapp.com/main.html", website:"Heroku"}]}
+                                />
+
+                        </Grid>
+                        
                     </Grid>
                 </div>
             </div>
