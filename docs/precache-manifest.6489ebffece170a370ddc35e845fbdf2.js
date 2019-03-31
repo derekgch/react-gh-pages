b867b6d3794286b44caecc4911d0db652d987ffe
Derek Chen <derekgc@icloud.com>
2019-03-30 22:06:59 -0400

    updated the name div

---
+++ docs/precache-manifest.6489ebffece170a370ddc35e845fbdf2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "e99a2dd745bd891f1a759983ce149160",
+    "url": "/static/media/stockfinder.e99a2dd7.png"
+  },
+  {
+    "revision": "2e679f1c46b2f01619a4",
+    "url": "/static/css/main.2997f409.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8a6fe1b800dd8c52a52e",
+    "url": "/static/js/2.b6e245c9.chunk.js"
+  },
+  {
+    "revision": "7117cc5bb00cdd85c3b47b54c36793b8",
+    "url": "/static/media/typing.7117cc5b.png"
+  },
+  {
+    "revision": "2e679f1c46b2f01619a4",
+    "url": "/static/js/main.718a439f.chunk.js"
+  },
+  {
+    "revision": "effe2a6bc8f58eeecc2499d03d1a758c",
+    "url": "/static/media/play.effe2a6b.png"
+  },
+  {
+    "revision": "fa49d9fee8b4df929f4d655e04d89ee2",
+    "url": "/static/media/stock-simulator.fa49d9fe.png"
+  },
+  {
+    "revision": "6e7ddb1c3f1ef156e5f39d7805e47c3f",
+    "url": "/static/media/derekchen.6e7ddb1c.jpg"
+  },
+  {
+    "revision": "988d73a8f855d0d6388659e8201b54e5",
+    "url": "/static/media/Santorini.988d73a8.jpg"
+  },
+  {
+    "revision": "ce13fd663c411e4f46ec49c1e1014960",
+    "url": "/index.html"
+  }
+];
--- src/components/Name.js
+++ src/components/Name.js
@@ -1,30 +1,91 @@
+import React, { useRef, useState, useEffect, useCallback } from 'react'
+import { useTransition, animated } from 'react-spring'
+
+function Name() {
+  const ref = useRef([])
+  const [items, set] = useState([])
+  const transitions = useTransition(items, null, {
+    from: { opacity: 0, height: 0, innerHeight: 0, transform: 'perspective(600px) rotateX(0deg)', color: '#8fa5b6' },
+    enter: [
+      { opacity: 1, height: 80, innerHeight: 80 },
+      { transform: 'perspective(600px) rotateX(360deg)', color: '#28d79f' },
+      { transform: 'perspective(600px) rotateX(0deg)' },
+    ],
+    leave: [{ color: '#c23369' }, { innerHeight: 0 }, { opacity: 0, height: 0 }],
+    update: { color: '#28b4d7' },
+  })
+
+  const reset = useCallback(() => {
+    ref.current.map(clearTimeout)
+    ref.current = []
+    set([])
+    ref.current.push(setTimeout(() => set(['Full Stack Developer']), 2000))
+    ref.current.push(setTimeout(() => set(['Software Engineer']), 5000))
+    ref.current.push(setTimeout(() => set(['Derek Guo Chen']), 8000))
+  }, [])
+
+  useEffect(() => void reset(), [])
+
+  return (
+    <div className="name-title">
+      {transitions.map(({ item, props: { innerHeight, ...rest }, key }) => (
+        <animated.div className="transitions-item" key={key} style={rest} onClick={reset}>
+          <animated.div style={{ overflow: 'hidden', height: innerHeight }}>{item}</animated.div>
+        </animated.div>
+      ))}
+    </div>
+  )
+}
+
+export default Name;
+
+
+
 // import React from 'react';
 // import PropTypes from 'prop-types';
 // import { withStyles } from '@material-ui/core/styles';
-// import Paper from '@material-ui/core/Paper';
+// import Card from '@material-ui/core/Card';
+// import CardContent from '@material-ui/core/CardContent';
+// // import Button from '@material-ui/core/Button';
 // import Typography from '@material-ui/core/Typography';
+// import EmailIcon from '@material-ui/icons/Email';
+// import SvgIcon from '@material-ui/core/SvgIcon';
 
-// const styles = theme => ({
-//   root: {
-//     ...theme.mixins.gutters(),
-//     paddingTop: theme.spacing.unit * 2,
-//     paddingBottom: theme.spacing.unit * 2,
+
+// const styles = {
+//   card: {
+//     minWidth: 275,
 //   },
-// });
+//   bullet: {
+//     display: 'inline-block',
+//     margin: '0 2px',
+//     transform: 'scale(0.8)',
+//   },
+//   title: {
+//     fontSize: 14,
+//   },
+//   pos: {
+//     marginBottom: 12,
+//   },
+// };
 
 // function Name(props) {
 //   const { classes } = props;
 
 //   return (
-//     <div>
-//       <Paper className={classes.root} elevation={1} style={{background:"transparent"}}>
-//         <Typography variant="h3" component="h3">
-//           Derek Guo Chen
+//     <div className="" id="home" >
+//     <Card className={classes.card} style={{background:"transparent", color:"black", boxShadow:"none"}}>
+//       <CardContent>
+//       <Typography variant="h3" component="h3">
+//             Derek G Chen
 //         </Typography>
-//         <Typography variant="h5">
-//             Full-Stack Web Developer
-//         </Typography>
-//       </Paper>
+
+//       <Typography variant="h6" component="h3">
+//             Full-Stack Web Application Developer
+//           </Typography>
+//       </CardContent>
+
+//     </Card>
 //     </div>
 //   );
 // }
@@ -34,60 +95,3 @@
 // };
 
 // export default withStyles(styles)(Name);
-
-
-
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-// import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import EmailIcon from '@material-ui/icons/Email';
-import SvgIcon from '@material-ui/core/SvgIcon';
-
-
-const styles = {
-  card: {
-    minWidth: 275,
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-};
-
-function Name(props) {
-  const { classes } = props;
-
-  return (
-    <div className="" id="home" >
-    <Card className={classes.card} style={{background:"transparent", color:"black", boxShadow:"none"}}>
-      <CardContent>
-      <Typography variant="h3" component="h3">
-            Derek G Chen
-        </Typography>
-
-      <Typography variant="h6" component="h3">
-            Full-Stack Web Application Developer
-          </Typography>
-      </CardContent>
-
-    </Card>
-    </div>
-  );
-}
-
-Name.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(Name);
