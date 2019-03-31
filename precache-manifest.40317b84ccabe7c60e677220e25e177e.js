7cf6f2999b5ddb3c735ffe37b9dc8e5d7255d03d
Derek Chen <derekgc@icloud.com>
2019-03-30 22:08:21 -0400

    Updates

---
+++ precache-manifest.40317b84ccabe7c60e677220e25e177e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "e99a2dd745bd891f1a759983ce149160",
+    "url": "/static/media/stockfinder.e99a2dd7.png"
+  },
+  {
+    "revision": "dfd76ab2e5d9249234b6",
+    "url": "/static/css/main.0641a254.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "333a52967eacb3434418",
+    "url": "/static/js/2.2b68e54d.chunk.js"
+  },
+  {
+    "revision": "7117cc5bb00cdd85c3b47b54c36793b8",
+    "url": "/static/media/typing.7117cc5b.png"
+  },
+  {
+    "revision": "dfd76ab2e5d9249234b6",
+    "url": "/static/js/main.af2b6e8e.chunk.js"
+  },
+  {
+    "revision": "effe2a6bc8f58eeecc2499d03d1a758c",
+    "url": "/static/media/play.effe2a6b.png"
+  },
+  {
+    "revision": "fa49d9fee8b4df929f4d655e04d89ee2",
+    "url": "/static/media/stock-simulator.fa49d9fe.png"
+  },
+  {
+    "revision": "6e7ddb1c3f1ef156e5f39d7805e47c3f",
+    "url": "/static/media/derekchen.6e7ddb1c.jpg"
+  },
+  {
+    "revision": "988d73a8f855d0d6388659e8201b54e5",
+    "url": "/static/media/Santorini.988d73a8.jpg"
+  },
+  {
+    "revision": "67bf19a3056db7af994313f22109baa5",
+    "url": "/index.html"
+  }
+];
