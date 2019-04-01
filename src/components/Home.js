4966d01f3d97afb3422bfe9dc31e7a869872bddb
Derek Chen <derekgc@icloud.com>
2019-03-31 22:07:24 -0400

    start adding transition

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,14 +5,6 @@ class Home extends Component {
     render() {
         return (
             <div className='front' id="home">
-                {/* <img src={Background} className="background-home" width="auto"/> */}
-                {/* <div className="name-title">
-                <h1 >Derek Guo Chen</h1>
-                <h2 >Full Stack Web Developer</h2>
-                </div> */}
-
-
-
             </div>
         );
     }
--- src/components/ProjectModal.js
+++ src/components/ProjectModal.js
@@ -1,81 +1,27 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import Modal from '@material-ui/core/Modal';
-import Button from '@material-ui/core/Button';
+import React, { useState, useCallback } from 'react'
+import { useTransition, animated } from 'react-spring'
+import './styles.css'
 
-function rand() {
-  return Math.round(Math.random() * 20) - 10;
+const pages = [
+  ({ style }) => <animated.div style={{ ...style, background: 'lightpink' }}>A</animated.div>,
+  ({ style }) => <animated.div style={{ ...style, background: 'lightblue' }}>B</animated.div>,
+  ({ style }) => <animated.div style={{ ...style, background: 'lightgreen' }}>C</animated.div>,
+]
+
+export default function ProjectModal() {
+  const [index, set] = useState(0)
+  const onClick = useCallback(() => set(state => (state + 1) % 3), [])
+  const transitions = useTransition(index, p => p, {
+    from: { opacity: 0, transform: 'translate3d(100%,0,0)' },
+    enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
+    leave: { opacity: 0, transform: 'translate3d(-50%,0,0)' },
+  })
+  return (
+    <div className="simple-trans-main" onClick={onClick}>
+      {transitions.map(({ item, props, key }) => {
+        const Page = pages[item]
+        return <Page key={key} style={props} />
+      })}
+    </div>
+  )
 }
-
-function getModalStyle() {
-  const top = 50 + rand();
-  const left = 50 + rand();
-
-  return {
-    top: `${top}%`,
-    left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`,
-  };
-}
-
-const styles = theme => ({
-  paper: {
-    position: 'absolute',
-    width: theme.spacing.unit * 50,
-    backgroundColor: theme.palette.background.paper,
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing.unit * 4,
-  },
-});
-
-class SimpleModal extends React.Component {
-  state = {
-    open: false,
-  };
-
-  handleOpen = () => {
-    this.setState({ open: true });
-  };
-
-  handleClose = () => {
-    this.setState({ open: false });
-  };
-
-  render() {
-    const { classes } = this.props;
-
-    return (
-      <div>
-        <Typography gutterBottom>Click to get the full Modal experience!</Typography>
-        <Button onClick={this.handleOpen}>Open Modal</Button>
-        <Modal
-          aria-labelledby="simple-modal-title"
-          aria-describedby="simple-modal-description"
-          open={this.state.open}
-          onClose={this.handleClose}
-        >
-          <div style={getModalStyle()} className={classes.paper}>
-            <Typography variant="h6" id="modal-title">
-              Text in a modal
-            </Typography>
-            <Typography variant="subtitle1" id="simple-modal-description">
-              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-            </Typography>
-            <SimpleModalWrapped />
-          </div>
-        </Modal>
-      </div>
-    );
-  }
-}
-
-SimpleModal.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-// We need an intermediary variable for handling the recursive nesting.
-const SimpleModalWrapped = withStyles(styles)(SimpleModal);
-
-export default SimpleModalWrapped;
